9739b8bc81a900bddc7da5a03ec3da14c3fba2ee
King-Mikaelson <anazodomichael27@gmail.com>
2023-06-09 20:44:20 +0100

    first commit

--- src/components/swiper/index.jsx
+++ src/components/swiper/index.jsx
@@ -27,7 +27,7 @@ function Carousel() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         pagination={{
