441715d9bf5d35abe14dfa2ef185e4cca49b6b6a
King-Mikaelson <anazodomichael27@gmail.com>
2023-06-10 13:17:29 +0100

    modified carousel

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -16,28 +16,28 @@ function Hero() {
 <div className="hidden md:block absolute z-10 -bottom-36  bg-[url(/Vector2.png)] h-[50rem] w-full bg-center bg-cover bg-no-repeat"></div>
     </div>
 <div className="relative">
-<Image src={download} alt="download" className="w-[5rem] h-[5rem] lg:w-[6.5rem] lg:h-[6.5rem] object-contain absolute bottom-[1rem]  lg:right-[47%] lg:left-[47%] right-[42%] left-[42%]" />
+<Image priority={true} src={download} alt="download" className="w-[5rem] h-[5rem] lg:w-[6.5rem] lg:h-[6.5rem] object-contain absolute bottom-[1rem]  lg:right-[47%] lg:left-[47%] right-[42%] left-[42%]" />
 </div>
 <div className="flex flex-col items-center md:items-stretch">
 <div className="grid grid-cols-2 md:flex items-center flex-col md:flex-row justify-around mx-4  lg:mx-48 xl:gap-14 lg:gap-1 gap-3 ">
-  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" />
-  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={voya} alt="marketList" className="w-[7rem] h-[7rem] object-contain" />
-  <Image src={eMenu} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" />
-  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
+  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain"priority={true} />
+  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={voya} alt="marketList" className="w-[7rem] h-[7rem] object-contain" priority={true}/>
+  <Image src={eMenu} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" priority={true}/>
+  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
 </div>
 <div className="grid grid-cols-2 md:flex  items-center flex-col md:flex-row  justify-around  mx-4  lg:mx-48 xl:gap-14 lg:gap-1 gap-3 border-b-2 border-[#EEEEEE] solid pb-12">
-  <Image src={voya} alt="marketList" className="w-[7rem] h-[7rem] object-contain" />
-  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={goMarket} alt="marketList" className="w-[3rem] h-[3rem] object-contain" />
-  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" />
-  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
-  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" />
-  <Image src={eMenu} alt="marketList" className="w-[5rem] h-[5rem] object-contain" />
+  <Image src={voya} alt="marketList" className="w-[7rem] h-[7rem] object-contain" priority={true}/>
+  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={goMarket} alt="marketList" className="w-[3rem] h-[3rem] object-contain" priority={true}/>
+  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" priority={true}/>
+  <Image src={myNada} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={goMarket} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
+  <Image src={marketList} alt="marketList" className="w-[3rem] h-[3rem] object-contain" priority={true}/>
+  <Image src={eMenu} alt="marketList" className="w-[5rem] h-[5rem] object-contain" priority={true}/>
 </div>
 </div>
 
