d1d074d616e9a454d4c5e9508c73ba6638ff17a4
King-Mikaelson <anazodomichael27@gmail.com>
2023-10-31 18:00:35 +0100

    under construction webpage

---
+++ src/components/LoadingIndicator/index.jsx
@@ -0,0 +1,9 @@
+function LoadingIndicator() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
